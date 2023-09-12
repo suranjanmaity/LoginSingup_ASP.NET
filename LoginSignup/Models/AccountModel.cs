7537be4442b20f886f5c85a774fa53d08d53926f
suranjan.maity <suranjan.maity@NOI-PF3THR26>
2023-09-12 16:47:49 +0530

    Add project files.

---
+++ LoginSignup/Models/AccountModel.cs
@@ -0,0 +1,21 @@
+﻿namespace LoginSignup.Models
+{
+    public class AccountModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+        public List<string> Hobbies { get; set; }
+        public SourceOfIncome SourceOfIncome { get; set; }
+        public int Income { get; set; }
+        public int Age { get; set; }
+        public string Bio { get; set; }
+    }
+    public enum SourceOfIncome
+    {
+        Employed,Freelancer,Other
+    }
+}
---
+++ LoginSignup/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoginSignup.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

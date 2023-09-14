606ccf583ad8cb45b05ed8add3239728db2519ee
suranjanmaity <suranjanmaity4@gmai.com>
2023-09-14 16:54:31 +0530

    Added viewmodel

---
+++ LoginSignup/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginSignup.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Email is required")]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+        [Required(ErrorMessage ="Please enter Password")]
+        [PasswordPropertyText]
+        public string Password { get; set; } = null!;
+    }
+}
